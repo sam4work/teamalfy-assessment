1b70d5119b66ab88316d9f02eb69be14ba712838
sam4work <sam4work10@gmail.com>
2022-08-11 00:09:51 +0000

    Update FortifyServiceProvider.php

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -6,8 +6,10 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
@@ -36,10 +38,18 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
-        RateLimiter::for('login', function (Request $request) {
-            $email = (string) $request->email;
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', '=', $request->email)
+                ->orWhere('username', '=', $request->email)->first();
 
-            return Limit::perMinute(5)->by($email.$request->ip());
+            return ($user && Hash::check($request->password, $user->password))
+                ? $user : null;
+        });
+
+        RateLimiter::for('login', function (Request $request) {
+            $email = (string)$request->email;
+
+            return Limit::perMinute(5)->by($email . $request->ip());
         });
 
         RateLimiter::for('two-factor', function (Request $request) {
